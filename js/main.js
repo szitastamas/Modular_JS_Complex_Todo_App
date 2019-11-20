e891b01345a53122055f07f51b0d4ddd80704c84
ita3-tn08 <ita3-tn08@drheuer.net>
2019-11-20 14:13:19 +0100

    Added delete and status update

--- js/main.js
+++ js/main.js
@@ -1,53 +1,124 @@
+var Local_Storage = /** @class */ (function () {
+    function Local_Storage() {
+    }
+    Local_Storage.saveToLocalStorage = function (todoArray) {
+        localStorage.setItem('todos', JSON.stringify(todoArray));
+    };
+    Local_Storage.getTodosFromLocalStorage = function () {
+        var todos = JSON.parse(localStorage.getItem('todos'));
+        return todos;
+    };
+    return Local_Storage;
+}());
+var ToDoRepository = /** @class */ (function () {
+    function ToDoRepository() {
+    }
+    ToDoRepository.allTodos = Local_Storage.getTodosFromLocalStorage() === null ? [] : Local_Storage.getTodosFromLocalStorage();
+    return ToDoRepository;
+}());
 var Todo = /** @class */ (function () {
     function Todo(title, todoBody) {
+        this.id = ToDoRepository.allTodos.length === 0 ? 0 : ToDoRepository.allTodos[ToDoRepository.allTodos.length - 1].id + 1;
         this.title = title;
         this.todoBody = todoBody;
         this.creationDate = Date.now();
         this.isFinished = false;
     }
-    Todo.prototype.finishTodo = function () {
-        this.isFinished = true;
-    };
     return Todo;
 }());
 var UI = /** @class */ (function () {
     function UI() {
     }
-    UI.prototype.createTodo = function (todo) {
-        var tr = document.createElement("tr");
-        tr.classList.add("todo-item");
-        tr.id = "todo-" + (todoTableBody.querySelectorAll(".todo-item").length + 1);
+    UI.prototype.paintOutTodo = function (todo) {
+        var tr = document.createElement('tr');
+        tr.classList.add('todo-item');
+        tr.id = "todo-" + (todoTableBody.querySelectorAll('.todo-item').length + 1);
         var todoIcon = todo.isFinished
             ? '<i class="far fa-check-circle finished-icon status-icon"></i>'
             : '<i class="fas fa-clipboard-list unfinished-icon status-icon"></i>';
-        tr.innerHTML =
-            "\n            <td>" + todoIcon + "</td>\n            <td>" + todo.title + "</td>\n            <td>" + todo.todoBody + "</td>\n            <td><i class=\"far fa-times-circle delete-icon\"></i></td>\n        ";
+        tr.innerHTML = "\n            <td>" + todoIcon + "</td>\n            <td data-todo-id=\"" + todo.id + "\">" + todo.id + "</td>\n            <td>" + todo.title + "</td>\n            <td>" + todo.todoBody + "</td>\n            <td><i class=\"far fa-times-circle delete-icon\"></i></td>\n        ";
         todoTableBody.prepend(tr);
+    };
+    UI.prototype.displayMessage = function (message, className) {
+        var msgDiv = document.querySelector('.alert');
+        msgDiv.classList.add(className);
+        msgDiv.textContent = message;
+        setTimeout(function () {
+            msgDiv.classList.remove(className);
+        }, 1500);
+    };
+    UI.prototype.updateTodoStatus = function (todo) {
+        var todoTds = document.querySelectorAll('[data-todo-id]');
+        var toBeUpdatedTodoField = Array.from(todoTds).find(function (t) { return t.dataset.todoId == todo.id; }).previousElementSibling.firstChild;
+        if (todo.isFinished) {
+            toBeUpdatedTodoField.classList = 'far fa-check-circle finished-icon status-icon';
+        }
+        else {
+            toBeUpdatedTodoField.classList = 'fas fa-clipboard-list unfinished-icon status-icon';
+        }
+        Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
+    };
+    UI.prototype.deleteTodo = function (todo) {
+        var todoTds = document.querySelectorAll('[data-todo-id]');
+        var toBeDeletedField = Array.from(todoTds).find(function (t) { return t.dataset.todoId == todo.id; }).parentElement;
+        toBeDeletedField.remove();
     };
     return UI;
 }());
-var ToDoRepository = /** @class */ (function () {
-    function ToDoRepository() {
-    }
-    ToDoRepository.allTodos = [];
-    return ToDoRepository;
-}());
-var todoForm = document.getElementById("add-todo-form");
-var submitBtn = document.getElementById("add-todo-btn");
-var todoTableBody = document.getElementById("todo-table-body");
+var todoForm = document.getElementById('add-todo-form');
+var submitBtn = document.getElementById('add-todo-btn');
+var todoTableBody = document.getElementById('todo-table-body');
 // Submitting a ToDo
-todoForm.addEventListener("submit", function (e) {
+todoForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    var ui = new UI;
-    var todoTitle = document.getElementById("todo-title").value;
-    var todoBody = document.getElementById("todo-description").value;
-    var oneTodo = new Todo(todoTitle, todoBody);
-    ToDoRepository.allTodos.push(oneTodo);
-    console.log(oneTodo);
-    console.log(ToDoRepository.allTodos);
-    ui.createTodo(oneTodo);
-    document.querySelectorAll(".status-icon").forEach(function (i) { return i.addEventListener("click", statusFunction); });
+    var ui = new UI();
+    var todoTitle = document.getElementById('todo-title').value;
+    var todoBody = document.getElementById('todo-description').value;
+    if (validateTodoFields(todoTitle, todoBody)) {
+        var oneTodo = new Todo(todoTitle, todoBody);
+        ToDoRepository.allTodos.push(oneTodo);
+        Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
+        console.log(ToDoRepository.allTodos);
+        ui.displayMessage('Todo successfully added', 'success');
+        ui.paintOutTodo(oneTodo);
+        document.querySelectorAll('.status-icon').forEach(function (i) { return i.addEventListener('click', statusFunction); });
+        document.querySelectorAll('.delete-icon').forEach(function (i) { return i.addEventListener('click', removeTodo); });
+    }
+    else {
+        ui.displayMessage('Please fill in all fields!', 'error');
+    }
 });
-function statusFunction(e) {
-    console.log(e.target);
+function validateTodoFields(field1, field2) {
+    if (field1 === '' || field2 === '') {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
+function statusFunction(e) {
+    var ui = new UI();
+    var clickedTodoId = e.target.parentElement.nextElementSibling.textContent;
+    var clickedTodo = ToDoRepository.allTodos.find(function (todo) { return todo.id == clickedTodoId; });
+    clickedTodo.isFinished = !clickedTodo.isFinished;
+    ui.updateTodoStatus(clickedTodo);
+    ui.displayMessage("Todo's status changed to: " + (clickedTodo.isFinished ? 'Finished' : 'Unfinished'), 'success');
+    console.log(clickedTodo.isFinished ? 'Todo done!' : 'Todo is still to be finished...');
+}
+function removeTodo(e) {
+    var ui = new UI();
+    var clickedTodoId = e.target.parentElement.parentElement.children[1].dataset.todoId;
+    var clickedTodo = ToDoRepository.allTodos.find(function (todo) { return todo.id == clickedTodoId; });
+    ToDoRepository.allTodos.splice(ToDoRepository.allTodos.indexOf(clickedTodo), 1);
+    ui.deleteTodo(clickedTodo);
+    ui.displayMessage('Todo deleted.', 'success');
+    Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var ui = new UI();
+    if (ToDoRepository.allTodos.length !== 0) {
+        ToDoRepository.allTodos.forEach(function (todo) { return ui.paintOutTodo(todo); });
+        document.querySelectorAll('.status-icon').forEach(function (i) { return i.addEventListener('click', statusFunction); });
+        document.querySelectorAll('.delete-icon').forEach(function (i) { return i.addEventListener('click', removeTodo); });
+    }
+});
--- js/main.ts
+++ js/main.ts
@@ -1,87 +1,160 @@
-interface TodoModel{
-    title: string,
-    todoBody: string,
-    creationDate: number,
-    isFinished: boolean,
-    finishTodo: () => void
-}
-
-class Todo implements TodoModel{
+interface TodoModel {
+    id: number;
     title: string;
     todoBody: string;
     creationDate: number;
     isFinished: boolean;
-    constructor(title: string, todoBody: string){
+}
+
+class Local_Storage {
+    static saveToLocalStorage(todoArray: Todo[]): void {
+        localStorage.setItem('todos', JSON.stringify(todoArray));
+    }
+
+    static getTodosFromLocalStorage(): Todo[] {
+        const todos = JSON.parse(localStorage.getItem('todos'));
+
+        return todos;
+    }
+}
+
+class ToDoRepository {
+    public static allTodos: Todo[] = Local_Storage.getTodosFromLocalStorage() === null ? [] : Local_Storage.getTodosFromLocalStorage();
+}
+
+class Todo implements TodoModel {
+    id: number;
+    title: string;
+    todoBody: string;
+    creationDate: number;
+    isFinished: boolean;
+    constructor(title: string, todoBody: string) {
+        this.id = ToDoRepository.allTodos.length === 0 ? 0 : ToDoRepository.allTodos[ToDoRepository.allTodos.length - 1].id + 1;
         this.title = title;
         this.todoBody = todoBody;
         this.creationDate = Date.now();
         this.isFinished = false;
     }
-    
-    finishTodo(){
-        this.isFinished = true;
-    }
 }
 
-class UI{
-    createTodo(todo: Todo):void{
-        
-        let tr:HTMLTableRowElement = document.createElement("tr");
-        tr.classList.add("todo-item");
-        tr.id = `todo-${todoTableBody.querySelectorAll(".todo-item").length+1}`;
+class UI {
+    paintOutTodo(todo: Todo): void {
+        let tr: HTMLTableRowElement = document.createElement('tr');
+        tr.classList.add('todo-item');
+        tr.id = `todo-${todoTableBody.querySelectorAll('.todo-item').length + 1}`;
 
-        const todoIcon:string = 
-            todo.isFinished 
-                ? '<i class="far fa-check-circle finished-icon status-icon"></i>' 
-                : '<i class="fas fa-clipboard-list unfinished-icon status-icon"></i>';
+        const todoIcon: string = todo.isFinished
+            ? '<i class="far fa-check-circle finished-icon status-icon"></i>'
+            : '<i class="fas fa-clipboard-list unfinished-icon status-icon"></i>';
 
-        tr.innerHTML = 
-        `
+        tr.innerHTML = `
             <td>${todoIcon}</td>
+            <td data-todo-id="${todo.id}">${todo.id}</td>
             <td>${todo.title}</td>
             <td>${todo.todoBody}</td>
             <td><i class="far fa-times-circle delete-icon"></i></td>
         `;
-        
+
         todoTableBody.prepend(tr);
+    }
+
+    displayMessage(message: string, className: string): void {
+        const msgDiv: HTMLElement = document.querySelector('.alert');
+        msgDiv.classList.add(className);
+        msgDiv.textContent = message;
+
+        setTimeout(() => {
+            msgDiv.classList.remove(className);
+        }, 1500);
+    }
+
+    updateTodoStatus(todo: Todo): void {
+        let todoTds: NodeList = document.querySelectorAll('[data-todo-id]');
+
+        let toBeUpdatedTodoField = Array.from(todoTds).find(t => t.dataset.todoId == todo.id).previousElementSibling.firstChild;
+
+        if (todo.isFinished) {
+            toBeUpdatedTodoField.classList = 'far fa-check-circle finished-icon status-icon';
+        } else {
+            toBeUpdatedTodoField.classList = 'fas fa-clipboard-list unfinished-icon status-icon';
+        }
+
+        Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
+    }
+
+    deleteTodo(todo: Todo): void {
+        let todoTds: NodeList = document.querySelectorAll('[data-todo-id]');
+
+        let toBeDeletedField = Array.from(todoTds).find(t => t.dataset.todoId == todo.id).parentElement;
+
+        toBeDeletedField.remove();
     }
 }
 
-class ToDoRepository{
-    
-    public static allTodos:Todo[] = [];
+const todoForm: HTMLElement = document.getElementById('add-todo-form');
 
-}
+const submitBtn: HTMLElement = document.getElementById('add-todo-btn');
 
-const todoForm:HTMLElement = document.getElementById("add-todo-form");
-
-const submitBtn:HTMLElement = document.getElementById("add-todo-btn");
-
-const todoTableBody:HTMLElement = document.getElementById("todo-table-body");
-
+const todoTableBody: HTMLElement = document.getElementById('todo-table-body');
 
 // Submitting a ToDo
-todoForm.addEventListener("submit", e => {
+todoForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    const ui = new UI;
+    const ui = new UI();
 
-    const todoTitle:string = (<HTMLInputElement>document.getElementById("todo-title")).value
+    const todoTitle: string = (<HTMLInputElement>document.getElementById('todo-title')).value;
 
-    const todoBody:string = (<HTMLInputElement> document.getElementById("todo-description")).value;
+    const todoBody: string = (<HTMLInputElement>document.getElementById('todo-description')).value;
 
+    if (validateTodoFields(todoTitle, todoBody)) {
+        const oneTodo = new Todo(todoTitle, todoBody);
+        ToDoRepository.allTodos.push(oneTodo);
+        Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
 
-    const oneTodo = new Todo(todoTitle, todoBody);
-    ToDoRepository.allTodos.push(oneTodo);
+        console.log(ToDoRepository.allTodos);
+        ui.displayMessage('Todo successfully added', 'success');
+        ui.paintOutTodo(oneTodo);
+        (<NodeList>document.querySelectorAll('.status-icon')).forEach(i => i.addEventListener('click', statusFunction));
+        (<NodeList>document.querySelectorAll('.delete-icon')).forEach(i => i.addEventListener('click', removeTodo));
+    } else {
+        ui.displayMessage('Please fill in all fields!', 'error');
+    }
+});
 
-    console.log(oneTodo);
-    console.log(ToDoRepository.allTodos)
-
-    ui.createTodo(oneTodo);
-    (<NodeList>document.querySelectorAll(".status-icon")).forEach(i => i.addEventListener("click", statusFunction))
-
-})
-
-function statusFunction(e:any):void{
-    console.log(e.target);
+function validateTodoFields(field1: string, field2: string): boolean {
+    if (field1 === '' || field2 === '') {
+        return false;
+    } else {
+        return true;
+    }
 }
+
+function statusFunction(e: any): void {
+    const ui = new UI();
+    let clickedTodoId = e.target.parentElement.nextElementSibling.textContent;
+    const clickedTodo: Todo = ToDoRepository.allTodos.find(todo => todo.id == clickedTodoId);
+    clickedTodo.isFinished = !clickedTodo.isFinished;
+    ui.updateTodoStatus(clickedTodo);
+    ui.displayMessage(`Todo's status changed to: ${clickedTodo.isFinished ? 'Finished' : 'Unfinished'}`, 'success');
+    console.log(clickedTodo.isFinished ? 'Todo done!' : 'Todo is still to be finished...');
+}
+
+function removeTodo(e: any): void {
+    const ui = new UI();
+    let clickedTodoId = e.target.parentElement.parentElement.children[1].dataset.todoId;
+    const clickedTodo: Todo = ToDoRepository.allTodos.find(todo => todo.id == clickedTodoId);
+    ToDoRepository.allTodos.splice(ToDoRepository.allTodos.indexOf(clickedTodo), 1);
+    ui.deleteTodo(clickedTodo);
+    ui.displayMessage('Todo deleted.', 'success');
+    Local_Storage.saveToLocalStorage(ToDoRepository.allTodos);
+}
+
+document.addEventListener('DOMContentLoaded', (): void => {
+    let ui = new UI();
+    if (ToDoRepository.allTodos.length !== 0) {
+        ToDoRepository.allTodos.forEach(todo => ui.paintOutTodo(todo));
+        (<NodeList>document.querySelectorAll('.status-icon')).forEach(i => i.addEventListener('click', statusFunction));
+        (<NodeList>document.querySelectorAll('.delete-icon')).forEach(i => i.addEventListener('click', removeTodo));
+    }
+});
